e7cfa0f29349a0b5060ff1d7e949dcb6f946e43c
zxdfty1234 <60722966+zxdfty1234@users.noreply.github.com>
2021-02-22 16:44:10 +0900

    Crossbrowsing
    IE11

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 var mql = window.matchMedia("screen and (max-width: 768px)");
 
 // nav_Toggle 클릭시 navbar 내려오게
-const navbar = document.querySelector('#navbar');
-const toggle = document.querySelector('.nav_toggle');
-const sns = document.querySelector('.aside_sns');
-toggle.addEventListener('click', ()=>{
+var navbar = document.querySelector('#navbar');
+var toggle = document.querySelector('.nav_toggle');
+var sns = document.querySelector('.aside_sns');
+toggle.addEventListener('click', function(){
     if(mql.matches){
         navbar.style.top = 0;
     }else{
@@ -14,8 +14,8 @@ toggle.addEventListener('click', ()=>{
     }
 });
 // close 버튼 클릭시 navbar 올라가기
-const close = document.querySelector('.nav_close');
-close.addEventListener('click', ()=>{
+var close = document.querySelector('.nav_close');
+close.addEventListener('click', function(){
     if(mql.matches){
         navbar.style.top = -480 + 'px';
     }else{
@@ -24,18 +24,20 @@ close.addEventListener('click', ()=>{
     }
 });
 // scroll 하면 home 섹션 등장
-const home = document.querySelector('#home');
-const secTop = document.querySelector('#top');
-const conTop = document.querySelector('.home_content_top');
-const topHeight = secTop.getBoundingClientRect().height;
-document.addEventListener('scroll', ()=>{
+var home = document.querySelector('#home');
+var secTop = document.querySelector('#top');
+var conTop = document.querySelector('.home_content_top');
+var topHeight = secTop.getBoundingClientRect().height;
+document.addEventListener('scroll', function(){
     if(mql.matches){
+        if(window.pageYOffset > topHeight/4){
             home.style.opacity = 1;
             home.style.backgroundSize = 100 + '%';
             conTop.style.opacity = 1;
             conTop.style.top = 70 + 'px';
+        }
     }else{
-        if(window.scrollY > topHeight/4){
+        if(window.pageYOffset > topHeight/4){
             home.style.opacity = 1;
             home.style.backgroundSize = 100 + '%';
             conTop.style.opacity = 1;
@@ -44,12 +46,12 @@ document.addEventListener('scroll', ()=>{
     }
 });
 // scroll 하면 sybar, content left / right 등장
-document.addEventListener('scroll', ()=>{
-    const sybar = document.querySelector('.sybar');
-    const left = document.querySelector('.home_content_left');
-    const right = document.querySelector('.home_content_right');
+document.addEventListener('scroll', function(){
+    var sybar = document.querySelector('.sybar');
+    var left = document.querySelector('.home_content_left');
+    var right = document.querySelector('.home_content_right');
     if(mql.matches){
-        if(window.scrollY > 50){
+        if(window.pageYOffset > 50){
             sybar.style.height = 200 + 'px';
             left.style.opacity = 1;
             left.style.left = 0;
@@ -57,7 +59,7 @@ document.addEventListener('scroll', ()=>{
             right.style.right = 0;
         }
     }else{
-        if(window.scrollY > 550){
+        if(window.pageYOffset > 550){
             sybar.style.height = 200 + 'px';
             left.style.opacity = 1;
             left.style.left = 0;
@@ -67,27 +69,27 @@ document.addEventListener('scroll', ()=>{
     }
 });
 // scroll 하면 Arrow up 등장
-const arrow = document.querySelector('.aside_arrow');
-document.addEventListener('scroll', ()=>{
-    if(window.scrollY > topHeight/2){
+var arrow = document.querySelector('.aside_arrow');
+document.addEventListener('scroll', function(){
+    if(window.pageYOffset > topHeight/2){
         arrow.classList.add('visible');
     }else{
         arrow.classList.remove('visible');
     }
 });
 // arrow 누르면 최상단으로 이동
-arrow.addEventListener('click', ()=>{
+arrow.addEventListener('click', function(){
     window.scrollTo({top: 0, left: 0, behavior:'smooth'});
 });
 // 이미지를 순서대로 보여주는 함수(각 캠페인별로)
-let cnt = 0;
+var cnt = 0;
 var randomTimeout;
-const backImg = document.querySelector('.backImg');
-const bgArray = new Array();
+var backImg = document.querySelector('.backImg');
+var bgArray = new Array();
 function brRandombg(){
-    bgArray[0] = `url('images/1.jpg')`;
-    bgArray[1] = `url('images/2.jpg')`;
-    bgArray[2] = `url('images/3.jpg')`;
+    bgArray[0] = "url('images/1.jpg')";
+    bgArray[1] = "url('images/2.jpg')";
+    bgArray[2] = "url('images/3.jpg')";
     if(cnt < bgArray.length){
         backImg.style.backgroundImage=bgArray[cnt];
         randomTimeout = setTimeout(brRandombg,2000);
@@ -98,9 +100,9 @@ function brRandombg(){
     }
 };
 function imcRandombg(){
-    bgArray[0] = `url('images/4.jpg')`;
-    bgArray[1] = `url('images/5.jpg')`;
-    bgArray[2] = `url('images/6.jpg')`;
+    bgArray[0] = "url('images/4.jpg')";
+    bgArray[1] = "url('images/5.jpg')";
+    bgArray[2] = "url('images/6.jpg')";
     if(cnt < bgArray.length){
         backImg.style.backgroundImage=bgArray[cnt];
         randomTimeout = setTimeout(imcRandombg,2000);
@@ -111,9 +113,9 @@ function imcRandombg(){
     }
 };
 function tvRandombg(){
-    bgArray[0] = `url('images/7.jpg')`;
-    bgArray[1] = `url('images/8.jpg')`;
-    bgArray[2] = `url('images/9.jpg')`;
+    bgArray[0] = "url('images/7.jpg')";
+    bgArray[1] = "url('images/8.jpg')";
+    bgArray[2] = "url('images/9.jpg')";
     if(cnt < bgArray.length){
         backImg.style.backgroundImage=bgArray[cnt];
         randomTimeout = setTimeout(tvRandombg,2000);
@@ -124,9 +126,9 @@ function tvRandombg(){
     }
 };
 function snsRandombg(){
-    bgArray[0] = `url('images/10.jpg')`;
-    bgArray[1] = `url('images/11.jpg')`;
-    bgArray[2] = `url('images/12.jpg')`;
+    bgArray[0] = "url('images/10.jpg')";
+    bgArray[1] = "url('images/11.jpg')";
+    bgArray[2] = "url('images/12.jpg')";
     if(cnt < bgArray.length){
         backImg.style.backgroundImage=bgArray[cnt];
         randomTimeout = setTimeout(snsRandombg,2000);
@@ -137,14 +139,14 @@ function snsRandombg(){
     }
 };
 // capaign 마우스 올리면 배경에 이미지 나타냄
-const one = document.querySelector('.one').firstElementChild;
-const two = document.querySelector('.two').firstElementChild;
-const three = document.querySelector('.three').firstElementChild;
-const four = document.querySelector('.four').firstElementChild;
-const five = document.querySelector('.five').firstElementChild;
-const six = document.querySelector('.six').firstElementChild;
-document.addEventListener('mouseover', (event)=>{
-    const target = event.target;
+var one = document.querySelector('.one').firstElementChild;
+var two = document.querySelector('.two').firstElementChild;
+var three = document.querySelector('.three').firstElementChild;
+var four = document.querySelector('.four').firstElementChild;
+var five = document.querySelector('.five').firstElementChild;
+var six = document.querySelector('.six').firstElementChild;
+document.addEventListener('mouseover', function(event){
+    var target = event.target;
     switch(target){
         case two: brRandombg(); break;
         case three: imcRandombg(); break;
